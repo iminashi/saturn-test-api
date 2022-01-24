4509f557e2c053be394be57c8e28d38f416ecfd7
Tapio Malmberg <tapio.malmberg@gmail.com>
2022-01-24 18:52:47 +0200

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿open Saturn
+open Giraffe
+
+let getAll : HttpHandler =
+    fun next ctx ->
+        task {
+            let! data = Db.getAll ()
+            return! json data next ctx
+        }
+
+let apiRouter = router {
+    not_found_handler (setStatusCode 404 >=> text "Not found.")
+    get "/" getAll
+}
+
+let app = application {
+    use_gzip
+    use_router apiRouter
+}
+
+run app
